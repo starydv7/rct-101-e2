2954fc4ba58bc2692aa786c6d8cae327691eb425
starydv7 <pawanstar72926@gmail.com>
2022-05-30 18:15:51 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Products from "./components/Products";
 
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+  return <div>
+   
+    <Products />
+     
+
+  </div>;
 };
 
 export default App;
